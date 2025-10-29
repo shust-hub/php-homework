38f50de80ad199f9b2386dcb83362dc8176fe28f
ASH <alyonashustov@yandex.ru>
2025-10-29 23:12:05 +0300

    Исправление домашней работы 003

--- 003-operators/index.php
+++ 003-operators/index.php
@@ -6,14 +6,14 @@ $lastname = trim(fgets(STDIN));
 echo "Введите Отчество:";
 $fathername = trim(fgets(STDIN));
 
-$name = ucfirst($name);
-$lastname = ucfirst($lastname);
-$fathername = ucfirst($fathername);
+$name = mb_ucfirst(mb_strtolower($name));
+$lastname = mb_ucfirst(mb_strtolower($lastname));
+$fathername = mb_ucfirst(mb_strtolower($fathername));
 
 $fullname = $lastname . ' ' . $name . ' ' . $fathername;
-$initials = strtoupper($name[0]) . '.' . strtoupper($fathername[0]) . '.';
+$initials = $name[0] . '.' .$fathername[0] . '.';
 $lastnameAndInitials = $lastname . ' ' . $initials;
-$fio = strtoupper($lastname[0]) . strtoupper($name[0]) . strtoupper($fathername[0]);
+$fio = $lastname[0] . $name[0] . $fathername[0];
 
 echo "Полное ФИО: $fullname\n";
 echo "Инициалы: $lastnameAndInitials\n";
